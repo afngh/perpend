5e9ac0c0d40acd4bd0e61b880f19af034d7bbc83
Shaik Afnan <shaikafnan095@gmail.com>
2026-07-01 22:04:06 +0530

    add react routes for the required project project setup complete

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import NotFound from './components/NotFound';
+import Main from './components/Main';
+import Login from './components/Login';
+import Home from './components/Home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/home',
+    element: <Home />,
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  }
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
