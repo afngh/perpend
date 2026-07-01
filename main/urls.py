90a4ea5ac63524b4d41c54c7e2be202d23e92f15
Shaik Afnan <shaikafnan095@gmail.com>
2026-07-01 20:56:34 +0530

    add initial django project rename project##

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for perpendbackend project.
+URL configuration for main project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
