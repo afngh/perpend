d735bdb082c4ef506208b7c9c7506c44567fca85
Shaik Afnan <shaikafnan095@gmail.com>
2026-07-02 10:53:26 +0530

    add django model to for users and todos and subtopics

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, MainTopic, Todo
 
-# Register your models here.
+admin.site.register(User, UserAdmin)
+
+@admin.register(MainTopic)
+class MainTopicAdmin(admin.ModelAdmin):
+    list_display = ('domain', 'topic_id', 'title', 'user', 'priority')
+    list_filter = ('domain', 'priority', 'level', 'user')
+    search_fields = ('title', 'topic_id', 'domain', 'note')
+
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('subtopic_id', 'title', 'main_topic', 'status', 'completed')
+    list_filter = ('status', 'completed', 'main_topic__domain', 'main_topic__user')
+    search_fields = ('title', 'subtopic_id', 'notes', 'github_url')
